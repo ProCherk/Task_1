d9772951946fae36cded06c9a3f47053d8b5fa41
Богдан Македонський <makedonskyi.b@donnu.edu.ua>
2020-09-18 23:13:20 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Для виведення літери і, потрібно змінити кодування на Unocode */
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Привіт, ДонНУ!");
+        }
+    }
+}
